ed89e16cf6262ba6928f267fac3a7d8e684ed7fe
Alexandra <alexandravolchihina@yandex.ru>
2023-03-13 11:55:47 +0300

    Homework done

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ import java.util.stream.Stream;
 @RequestMapping("student")
 public class StudentController {
     private final StudentService studentService;
-
+//@Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
